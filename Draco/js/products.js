fba31fc74c6b1bf3194a9ed743f554c76b0ece26
FINISTER47 <47finister@gmail.com>
2025-05-14 03:25:52 -0600

    productos y categorias ajustados

---
+++ Draco/js/products.js
@@ -0,0 +1,155 @@
+// js/products.js
+export const products = {
+  "producto1": {
+    title: "Zapato de Gala Negro",
+    price: "MXN$ 7,900",
+    category: "Business",
+    description: "Zapato de gala hecho con piel genuina, suela de cuero y acabados artesanales. Elegancia clásica con comodidad moderna.",
+    images: [
+      "Media/producto1_3.png",
+      "Media/producto1_5.png",
+      "Media/producto1_4.png",
+      "Media/producto1_6.png"
+    ]
+  },
+  "producto2": {
+    title: "Zapato Oxford Caramelo",
+    price: "MXN$ 8,200",
+    category: "Casual",
+    description: "Zapato Oxford en tono caramelo, elaborado con piel pulida de acabado suave. Su diseño clásico y su color cálido lo convierten en una opción refinada para quienes buscan estilo sin sacrificar sobriedad.",
+    images: [
+      "Media/producto2.png",
+      "Media/producto2_1.png",
+      "Media/producto2_2.png",
+      "Media/producto2_3.png"
+    ]
+  },
+  "producto3": {
+    title: "Zapato Monk Strap de Piel",
+    price: "MXN$ 8,750",
+    category: "Monk Strap",
+    description: "Zapato monkstrap de una hebilla, hecho con piel negra de grano fino. Su silueta estructurada y su ajuste firme reflejan un equilibrio perfecto entre tradición, seguridad y elegancia atemporal.",
+    images: [
+      "Media/producto3.png",
+      "Media/producto3_1.png",
+      "Media/producto3_2.png",
+      "Media/producto3_3.png"
+    ]
+  },
+  "producto4": {
+    title: "Zapato Slip-On Texturizado",
+    price: "MXN$ 6,500",
+    category: "Slip-On",
+    description: "Zapato slip-on sin cordones, con acabado texturizado y estructura flexible. Comodidad absoluta con un diseño moderno que añade carácter a cualquier atuendo formal o casual elegante.",
+    images: [
+      "Media/producto4.png",
+      "Media/producto4_1.png",
+      "Media/producto4_2.png",
+      "Media/producto4_3.png"
+    ]
+  },
+  "producto5": {
+    title: "Oxford negro clásico",
+    price: "MXN$ 10,200",
+    category: "Business",
+    description: "Zapato Oxford en tono negro, elaborado con piel pulida de acabado suave. Diseño clásico y color Satinado.",
+    images: [
+      "Media/producto5.jpg",
+      "Media/producto5_1.jpg",
+      "Media/producto5_2.jpg"
+    ]
+  },
+  "producto6": {
+    title: "Oxford Oxford marrón",
+    price: "MXN$ 8,200",
+    category: "Casual",
+    description: "Zapato Oxford en tono marrón, elaborado con piel pulida de acabado suave. Diseño clásico y color obscuro.",
+    images: [
+      "Media/producto6.jpg",
+      "Media/producto6_1.jpg",
+      "Media/producto6_2.jpg",
+      "Media/producto6_3.jpg"
+    ]
+  },
+  "producto7": {
+    title: "Oxford negro estilizado",
+    price: "MXN$ 12,200",
+    category: "Ceremonia",
+    description: "Zapato Oxford en tono negro, elaborado con piel pulida de acabado suave. Diseño clásico y color Satinado.",
+    images: [
+      "Media/producto7.jpg",
+      "Media/producto7_1.jpg",
+      "Media/producto7_2.jpg",
+      "Media/producto7_3.jpg"
+    ]
+  },
+  "producto8": {
+    title: "Wingtip de Charol Negro",
+    price: "MXN$ 12,200",
+    category: "Ceremonia",
+    description: "Zapato Oxford en tono negro, elaborado con piel pulida de acabado suave. Diseño clásico y color Satinado.",
+    images: [
+      "Media/producto8.jpg",
+      "Media/producto8_1.jpg",
+      "Media/producto8_2.jpg"
+    ]
+  },
+  "producto9": {
+    title: "Oxfor/Wingtip Brillante",
+    price: "MXN$ 12,200",
+    category: "Casual",
+    description: "Zapato Oxford en tono negro, elaborado con piel pulida de acabado suave. Diseño clásico y color Satinado.",
+    images: [
+      "Media/producto9.jpg",
+      "Media/producto9_1.jpg",
+      "Media/producto9_2.jpg"
+    ]
+  },
+  "producto10": {
+    title: "Oxford Charol Negro",
+    price: "MXN$ 12,200",
+    category: "Ceremonia",
+    description: "Zapato Oxford en tono negro, elaborado con piel pulida de acabado suave. Diseño clásico y color Satinado.",
+    images: [
+      "Media/producto10.jpg",
+      "Media/producto10_1.jpg",
+      "Media/producto10_2.jpg",
+      "Media/producto10_3.jpg"
+    ]
+  },
+  "producto11": {
+    title: "Wingtip Café Madera",
+    price: "MXN$ 12,200",
+    category: "Wingtip",
+    description: "Zapato Oxford en tono negro, elaborado con piel pulida de acabado suave. Diseño clásico y color Satinado.",
+    images: [
+      "Media/producto11.jpg",
+      "Media/producto11_1.jpg",
+      "Media/producto11_2.jpg"
+    ]
+  },
+  "producto12": {
+    title: "Wingtip Vino",
+    price: "MXN$ 12,200",
+    category: "Wingtip",
+    description: "Zapato Oxford en tono negro, elaborado con piel pulida de acabado suave. Diseño clásico y color Satinado.",
+    images: [
+      "Media/producto12.jpg",
+      "Media/producto12_1.jpg",
+      "Media/producto12_2.jpg",
+      "Media/producto12_3.jpg"
+    ]
+  },
+  "producto13": {
+    title: "Wingtip Gamúza",
+    price: "MXN$ 12,200",
+    category: "Wingtip",
+    description: "Zapato Oxford en tono negro, elaborado con piel pulida de acabado suave. Diseño clásico y color Satinado.",
+    images: [
+      "Media/producto13.jpg",
+      "Media/producto13_1.jpg",
+      "Media/producto13_2.jpg",
+      "Media/producto13_3.jpg"
+    ]
+  },
+};
